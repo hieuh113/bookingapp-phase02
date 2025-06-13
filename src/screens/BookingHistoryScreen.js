098e6491f54d1b5208da5e7ee4a2ffd2147896e5
dabug-369 <ducanh198369@gmail.com>
2025-06-13 12:36:29 +0700

    fix booking history screen

--- src/screens/BookingHistoryScreen.js
+++ src/screens/BookingHistoryScreen.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Icon } from '@rneui/themed';
-import { mockBookings } from '../data/mockBookings';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API_BASE_URL = 'http://192.168.0.105:3000';
 
